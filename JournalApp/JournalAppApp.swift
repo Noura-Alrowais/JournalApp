bdae89a9c2a8f77034d743bec5063b829d690ea5
Noura <nalrowais24@twq.idserve.net>
2024-10-21 09:25:36 +0300

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Noura Alrowais on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
