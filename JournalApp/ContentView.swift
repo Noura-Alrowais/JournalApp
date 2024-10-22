56278641fad981598cd2f1d86060e25ee1b9a832
Noura <nalrowais24@twq.idserve.net>
2024-10-22 09:50:05 +0300

    First COde

--- JournalApp/ContentView.swift
+++ JournalApp/ContentView.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+           
+                Rectangle().fill(.linearGradient(colors:[Color(hex:"#141420"), Color(hex:"#000000")], startPoint: .top, endPoint: .bottom)).ignoresSafeArea()
+            VStack{
+                Image("book").resizable().scaledToFit().frame(width: 77.7, height: 101)
+                Text("Journali")
+                    .fontWeight(.black)
+                    .foregroundColor(Color.white).font(.system(size: 42))
+                Text("Your thoughts, your story").font(.system(size: 18)).foregroundColor(Color.white)
+            }
+            
+        }.ignoresSafeArea()
+     
+      
     }
 }
 
 #Preview {
     ContentView()
+}
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet(charactersIn: "#"))
+        let rgbValue = UInt32(hex, radix: 16)
+        let r = Double((rgbValue! & 0xFF0000) >> 16) / 255
+        let g = Double((rgbValue! & 0x00FF00) >> 8) / 255
+        let b = Double(rgbValue! & 0x0000FF) / 255
+        self.init(red: r, green: g, blue: b)
+    }
 }
